73a758335fca7e1c0e7a6f8c797e4ca0a3cf8947
XC <2277209447@qq.com>
2023-04-28 22:46:36 +0800

    Add writer's part

--- src/test/java/ImageDataParallelTest.java
+++ src/test/java/ImageDataParallelTest.java
@@ -10,7 +10,7 @@ public class ImageDataParallelTest {
         double[][] data = imageIO.read("D:\\Projects\\Java\\python-java\\band_clip_6.tif");
         List<double[]> centers = new ArrayList<>();
         centers.add(new double[]{0, 0, 0, 0, 0});
-        ParallelISODATA parallelISODATA = new ParallelISODATA(data, centers, 6, 1, 1, 4, 1, 10, 10);
+        ParallelISODATA parallelISODATA = new ParallelISODATA(data, centers, 6, data.length / 24, 1, 5, 1, 10, 10);
         long start = System.currentTimeMillis();
         int[] ans = parallelISODATA.getResult();
         long end = System.currentTimeMillis();
--- src/test/java/ImageDataTest.java
+++ src/test/java/ImageDataTest.java
@@ -10,10 +10,11 @@ public class ImageDataTest {
         double[][] data = imageIO.read("D:\\Projects\\Java\\python-java\\band_clip_6.tif");
         List<double[]> centers = new ArrayList<>();
         centers.add(new double[]{0, 0, 0, 0, 0});
-        ISODATA isodata = new ISODATA(data, centers, 6, 1, 1, 4, 1, 10);
+        ISODATA isodata = new ISODATA(data, centers, 6, data.length / 24, 1, 5, 1, 10);
         long start = System.currentTimeMillis();
         int[] ans = isodata.getResult();
         long end = System.currentTimeMillis();
         System.out.println("用时：" + (end - start));
+        imageIO.write(ans, "D:\\xx1.tif");
     }
 }
